6a388cb2538f87ad274ca9745d024b5c238483bc
VictorOduorke <victoroduorxh2@gmail.com>
2025-10-15 10:16:42 +0300

    changed register bussines from protected route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", protect, getProfile);
-router.post("/register-business", protect, registerBusiness);
+router.post("/register-business", registerBusiness);
 router.post("/logout", protect, logOutUser);
 export default router;
